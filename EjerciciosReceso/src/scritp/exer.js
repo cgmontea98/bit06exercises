0a5ad156037a93500f6a4a42f801bfce2abd61c9
cgmontea98 <cgmontealegre9804@gmail.com>
2023-01-07 23:00:18 -0500

    modificación a background, funcionalidad js en exer hasta el 7mo

--- EjerciciosReceso/src/scritp/exer.js
+++ EjerciciosReceso/src/scritp/exer.js
@@ -1,11 +1,28 @@
+//Ejercicio1
+let lis = document.getElementById("listo");
+lis.addEventListener("click", hola);
+
+function hola(e) {
+  e.preventDefault();
+  let nom = document.getElementById("nom").value;
+  let ape = document.getElementById("ape").value;
+  let ed = document.getElementById("ed").value;
+  if (ed >= 18) {
+    aler(`Bienvenido ${nom} ${ape}, su edad es ${ed} por lo tanto es mayor`);
+  } else {
+    alert(`Bienvenido ${nom} ${ape}, su edad es ${ed} por lo tanto es menor`);
+  }
+}
+
+//Ejercicio2
 //para opb
 let btn1 = Array.from(document.getElementsByClassName("vm1"));
 for (let i = 0; i < btn1.length; i++) {
   btn1[i].addEventListener("click", () => {
     let dis1 = document.querySelector(".opb--srmd");
     let mos1 = dis1.style.display;
-    if (mos1 === "block") {
-      dis1.style.display = "none";
+    if (mos1 === "none") {
+      dis1.style.display = "block";
     } else {
       dis1.style.display = "block";
     }
@@ -20,12 +37,75 @@ function mostrar() {
 
     if (op === "") {
       oper.style.display = "block";
-    } else{
-        oper.style.display = "";
+    } else {
+      oper.style.display = "";
     }
   });
 }
 mostrar();
+
+//Num
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+
+//Variable para los resultados
+let resultado;
+
+//Botones
+const mas = document.getElementById("mas");
+mas.addEventListener("click", (eS) => {
+  eS.preventDefault();
+
+  resultado = Number(num1.value) + Number(num2.value);
+  console.log(resultado);
+});
+
+const res = document.getElementById("res");
+res.addEventListener("click", (eR) => {
+  eR.preventDefault();
+
+  resultado = Number(num1.value) - Number(num2.value);
+  console.log(resultado);
+});
+
+const mul = document.getElementById("mul");
+mul.addEventListener("click", (eM) => {
+  eM.preventDefault();
+
+  resultado = Number(num1.value) * Number(num2.value);
+  console.log(resultado);
+});
+
+const vid = document.getElementById("vid");
+vid.addEventListener("click", (eV) => {
+  eV.preventDefault();
+
+  resultado = Number(num1.value) / Number(num2.value);
+  console.log(resultado);
+});
+
+const mod = document.getElementById("mod");
+mod.addEventListener("click", (em) => {
+  em.preventDefault();
+
+  resultado = Number(num1.value) % Number(num2.value);
+  console.log(resultado);
+});
+
+//Mostrar los resultados
+let resu = document.getElementById("resu");
+resu.addEventListener("click", alerta);
+function alerta(eRu) {
+  eRu.preventDefault();
+  alert(`El resultado de la operación es: ${resultado}`);
+}
+
+/*let resu = document.getElementById("resu");
+resu.addEventListener("click", (eRu) => {
+  eRu.preventDefault();
+  let imp = document.querySelector("resOb");
+  imp = resultado;
+}); */
 
 //Para ing num 3 a 17
 const btn = Array.from(document.getElementsByClassName("vmp"));
@@ -41,16 +121,27 @@ for (let i = 0; i < btn.length; i++) {
   });
 }
 
+//variable global para ejercicios del 3 al 17
+let numP = document.getElementById("numO");
+console.log(numP);
+
 //Para ver resultados
 let btn3 = Array.from(document.querySelectorAll(".vm3"));
 for (let i = 0; i < btn3.length; i++) {
   btn3[i].addEventListener("click", () => {
     let dis3 = document.querySelector(".res--3");
     let mos3 = dis3.style.display;
-    if (mos3 === "block") {
-      dis3.style.display = "none";
-    } else {
+    if (mos3 === "") {
       dis3.style.display = "block";
+      //operación
+      resultado = Number(numP.value) * 3;
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje3 = document.getElementById("eje3");
+      eje3.innerHTML = resultado;
+    } else {
+      dis3.style.display = "";
     }
   });
 }
@@ -60,10 +151,17 @@ for (let i = 0; i < btn4.length; i++) {
   btn4[i].addEventListener("click", () => {
     let dis4 = document.querySelector(".res--4");
     let mos4 = dis4.style.display;
-    if (mos4 === "block") {
-      dis4.style.display = "none";
-    } else {
+    if (mos4 === "none") {
       dis4.style.display = "block";
+      //operación
+      resultado = Number(numP.value) / 2;
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje4 = document.getElementById("eje4");
+      eje4.innerHTML = resultado;
+    } else {
+      dis4.style.display = "none";
     }
   });
 }
@@ -73,10 +171,17 @@ for (let i = 0; i < btn5.length; i++) {
   btn5[i].addEventListener("click", () => {
     let dis5 = document.querySelector(".res--5");
     let mos5 = dis5.style.display;
-    if (mos5 === "block") {
-      dis5.style.display = "none";
-    } else {
+    if (mos5 === "none") {
       dis5.style.display = "block";
+      //operación
+      resultado = Number(numP.value) / 3;
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje5 = document.getElementById("eje5");
+      eje5.innerHTML = resultado;
+    } else {
+      dis5.style.display = "none";
     }
   });
 }
@@ -86,10 +191,17 @@ for (let i = 0; i < btn6.length; i++) {
   btn6[i].addEventListener("click", () => {
     let dis6 = document.querySelector(".res--6");
     let mos6 = dis6.style.display;
-    if (mos6 === "block") {
-      dis6.style.display = "none";
-    } else {
+    if (mos6 === "none") {
       dis6.style.display = "block";
+      //operación
+      resultado = Number(numP.value) - 5;
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje6 = document.getElementById("eje6");
+      eje6.innerHTML = resultado;
+    } else {
+      dis6.style.display = "none";
     }
   });
 }
@@ -99,10 +211,17 @@ for (let i = 0; i < btn7.length; i++) {
   btn7[i].addEventListener("click", () => {
     let dis7 = document.querySelector(".res--7");
     let mos7 = dis7.style.display;
-    if (mos7 === "block") {
-      dis7.style.display = "none";
-    } else {
+    if (mos7 === "none") {
       dis7.style.display = "block";
+      //operación
+      resultado = Number(numP.value) * Number(numP.value);
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje7 = document.getElementById("eje7");
+      eje7.innerHTML = resultado;
+    } else {
+      dis7.style.display = "none";
     }
   });
 }
@@ -112,10 +231,18 @@ for (let i = 0; i < btn8.length; i++) {
   btn8[i].addEventListener("click", () => {
     let dis8 = document.querySelector(".res--8");
     let mos8 = dis8.style.display;
-    if (mos8 === "block") {
-      dis8.style.display = "none";
-    } else {
+    if (mos8 === "none") {
       dis8.style.display = "block";
+      //operación
+
+      resultado = "pte";
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje8 = document.getElementById("eje8");
+      eje8.innerHTML = resultado;
+    } else {
+      dis8.style.display = "none";
     }
   });
 }
@@ -125,10 +252,17 @@ for (let i = 0; i < btn9.length; i++) {
   btn9[i].addEventListener("click", () => {
     let dis9 = document.querySelector(".res--9");
     let mos9 = dis9.style.display;
-    if (mos9 === "block") {
-      dis9.style.display = "none";
-    } else {
+    if (mos9 === "none") {
       dis9.style.display = "block";
+      //operación
+      resultado = "pte";
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje9 = document.getElementById("eje9");
+      eje9.innerHTML = resultado;
+    } else {
+      dis9.style.display = "none";
     }
   });
 }
@@ -138,10 +272,17 @@ for (let i = 0; i < btn10.length; i++) {
   btn10[i].addEventListener("click", () => {
     let dis10 = document.querySelector(".res--10");
     let mos10 = dis10.style.display;
-    if (mos10 === "block") {
-      dis10.style.display = "none";
-    } else {
+    if (mos10 === "none") {
       dis10.style.display = "block";
+      //operación
+      resultado = "pte";
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje10 = document.getElementById("eje10");
+      eje10.innerHTML = resultado;
+    } else {
+      dis10.style.display = "none";
     }
   });
 }
@@ -151,10 +292,17 @@ for (let i = 0; i < btn11.length; i++) {
   btn11[i].addEventListener("click", () => {
     let dis11 = document.querySelector(".res--11");
     let mos11 = dis11.style.display;
-    if (mos11 === "block") {
-      dis11.style.display = "none";
-    } else {
+    if (mos11 === "none") {
       dis11.style.display = "block";
+      //operación
+      resultado = "pte";
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje11 = document.getElementById("eje11");
+      eje11.innerHTML = resultado;
+    } else {
+      dis11.style.display = "none";
     }
   });
 }
@@ -164,10 +312,17 @@ for (let i = 0; i < btn12.length; i++) {
   btn12[i].addEventListener("click", () => {
     let dis12 = document.querySelector(".res--12");
     let mos12 = dis12.style.display;
-    if (mos12 === "block") {
-      dis12.style.display = "none";
-    } else {
+    if (mos12 === "none") {
       dis12.style.display = "block";
+      //operación
+      resultado = "pte";
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje12 = document.getElementById("eje12");
+      eje12.innerHTML = resultado;
+    } else {
+      dis12.style.display = "none";
     }
   });
 }
@@ -177,10 +332,17 @@ for (let i = 0; i < btn13.length; i++) {
   btn13[i].addEventListener("click", () => {
     let dis13 = document.querySelector(".res--13");
     let mos13 = dis13.style.display;
-    if (mos13 === "block") {
-      dis13.style.display = "none";
-    } else {
+    if (mos13 === "none") {
       dis13.style.display = "block";
+      //operación
+      resultado = "pte";
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje13 = document.getElementById("eje13");
+      eje13.innerHTML = resultado;
+    } else {
+      dis13.style.display = "none";
     }
   });
 }
@@ -190,10 +352,17 @@ for (let i = 0; i < btn14.length; i++) {
   btn14[i].addEventListener("click", () => {
     let dis14 = document.querySelector(".res--14");
     let mos14 = dis14.style.display;
-    if (mos14 === "block") {
-      dis14.style.display = "none";
-    } else {
+    if (mos14 === "none") {
       dis14.style.display = "block";
+      //operación
+      resultado = "pte";
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje14 = document.getElementById("eje14");
+      eje14.innerHTML = resultado;
+    } else {
+      dis14.style.display = "none";
     }
   });
 }
@@ -203,10 +372,17 @@ for (let i = 0; i < btn15.length; i++) {
   btn15[i].addEventListener("click", () => {
     let dis15 = document.querySelector(".res--15");
     let mos15 = dis15.style.display;
-    if (mos15 === "block") {
-      dis15.style.display = "none";
-    } else {
+    if (mos15 === "none") {
       dis15.style.display = "block";
+      //operación
+      resultado = "pte";
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje15 = document.getElementById("eje15");
+      eje15.innerHTML = resultado;
+    } else {
+      dis15.style.display = "none";
     }
   });
 }
@@ -215,10 +391,17 @@ for (let i = 0; i < btn16.length; i++) {
   btn16[i].addEventListener("click", () => {
     let dis16 = document.querySelector(".res--16");
     let mos16 = dis16.style.display;
-    if (mos16 === "block") {
-      dis16.style.display = "none";
-    } else {
+    if (mos16 === "none") {
       dis16.style.display = "block";
+      //operación
+      resultado = "pte";
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje16 = document.getElementById("eje16");
+      eje16.innerHTML = resultado;
+    } else {
+      dis16.style.display = "none";
     }
   });
 }
@@ -228,10 +411,17 @@ for (let i = 0; i < btn17.length; i++) {
   btn17[i].addEventListener("click", () => {
     let dis17 = document.querySelector(".res--17");
     let mos17 = dis17.style.display;
-    if (mos17 === "block") {
-      dis17.style.display = "none";
-    } else {
+    if (mos17 === "none") {
       dis17.style.display = "block";
+      //operación
+      resultado = "pte";
+      //resultado
+      alert(`el resultado es ${resultado}`);
+      //impresión en html
+      let eje17 = document.getElementById("eje17");
+      eje17.innerHTML = resultado;
+    } else {
+      dis17.style.display = "none";
     }
   });
 }
