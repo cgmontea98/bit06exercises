f3967a43bb9a8f7ccac5812d17b3c7745027e469
cgmontea98 <cgmontealegre9804@gmail.com>
2023-01-10 18:17:11 -0500

    solo quedan los último sin realizar

--- EjerciciosReceso/src/scritp/exer.js
+++ EjerciciosReceso/src/scritp/exer.js
@@ -1,3 +1,4 @@
+"use strict";
 //Ejercicio1
 let lis = document.getElementById("listo");
 lis.addEventListener("click", hola);
@@ -8,7 +9,7 @@ function hola(e) {
   let ape = document.getElementById("ape").value;
   let ed = document.getElementById("ed").value;
   if (ed >= 18) {
-    aler(`Bienvenido ${nom} ${ape}, su edad es ${ed} por lo tanto es mayor`);
+    alert(`Bienvenido ${nom} ${ape}, su edad es ${ed} por lo tanto es mayor`);
   } else {
     alert(`Bienvenido ${nom} ${ape}, su edad es ${ed} por lo tanto es menor`);
   }
@@ -44,11 +45,11 @@ function mostrar() {
 }
 mostrar();
 
-//Num
+//Num para operaciones básicas
 let num1 = document.getElementById("num1");
 let num2 = document.getElementById("num2");
 
-//Variable para los resultados
+//Variable general para los resultados
 let resultado;
 
 //Botones
@@ -57,7 +58,8 @@ mas.addEventListener("click", (eS) => {
   eS.preventDefault();
 
   resultado = Number(num1.value) + Number(num2.value);
-  console.log(resultado);
+
+  resOb.innerHTML = resultado;
 });
 
 const res = document.getElementById("res");
@@ -65,15 +67,19 @@ res.addEventListener("click", (eR) => {
   eR.preventDefault();
 
   resultado = Number(num1.value) - Number(num2.value);
-  console.log(resultado);
+
+  resOb.innerHTML = resultado;
 });
 
 const mul = document.getElementById("mul");
 mul.addEventListener("click", (eM) => {
   eM.preventDefault();
 
+  resOb.innerHTML = resultado;
+
   resultado = Number(num1.value) * Number(num2.value);
-  console.log(resultado);
+
+  resOb.innerHTML = resultado;
 });
 
 const vid = document.getElementById("vid");
@@ -81,7 +87,8 @@ vid.addEventListener("click", (eV) => {
   eV.preventDefault();
 
   resultado = Number(num1.value) / Number(num2.value);
-  console.log(resultado);
+
+  resOb.innerHTML = resultado;
 });
 
 const mod = document.getElementById("mod");
@@ -89,7 +96,8 @@ mod.addEventListener("click", (em) => {
   em.preventDefault();
 
   resultado = Number(num1.value) % Number(num2.value);
-  console.log(resultado);
+
+  resOb.innerHTML = resultado;
 });
 
 //Mostrar los resultados
@@ -123,7 +131,6 @@ for (let i = 0; i < btn.length; i++) {
 
 //variable global para ejercicios del 3 al 17
 let numP = document.getElementById("numO");
-console.log(numP);
 
 //Para ver resultados
 let btn3 = Array.from(document.querySelectorAll(".vm3"));
@@ -234,13 +241,19 @@ for (let i = 0; i < btn8.length; i++) {
     if (mos8 === "none") {
       dis8.style.display = "block";
       //operación
-
-      resultado = "pte";
-      //resultado
-      alert(`el resultado es ${resultado}`);
-      //impresión en html
-      let eje8 = document.getElementById("eje8");
-      eje8.innerHTML = resultado;
+      function multiplos() {
+        for (let i = 0; i <= numP.value; i++) {
+          if (i % 2 === 0) {
+            resultado += i + ", ";
+            //resultado
+            alert(`el resultado es ${resultado}`);
+            //impresión en html
+            let eje8 = document.getElementById("eje8");
+            eje8.innerHTML = resultado;
+          }
+        }
+      }
+      multiplos();
     } else {
       dis8.style.display = "none";
     }
@@ -254,18 +267,39 @@ for (let i = 0; i < btn9.length; i++) {
     let mos9 = dis9.style.display;
     if (mos9 === "none") {
       dis9.style.display = "block";
-      //operación
-      resultado = "pte";
-      //resultado
-      alert(`el resultado es ${resultado}`);
-      //impresión en html
-      let eje9 = document.getElementById("eje9");
-      eje9.innerHTML = resultado;
+      //usando operador ternario
+      Number(numP.value % 2 == 0)
+        ? //SI ES TRUE
+          (alert(`el número ${Number(numP.value)} es PAR`),
+          (eje9.innerHTML = `el número ${Number(numP.value)} es PAR`))
+        : // SI ES FALSE
+          (alert(`el número ${Number(numP.value)} es IMPAR`),
+          (eje9.innerHTML = `el número ${Number(numP.value)} es IMPAR`));
+      /* op(); */
     } else {
       dis9.style.display = "none";
     }
   });
 }
+//otra opcion para ejercicio9
+/* function op() {
+  resultado = Number(numP.value) % 2;
+  if (resultado === 0) {
+    alert(`el número ${Number(numP.value)} es PAR`);
+    //impresión en html
+    let eje9 = document.getElementById("eje9");
+    eje9.innerHTML = `el número ${Number(numP.value)} es PAR`;
+  } else if (resultado >= 1) {
+    alert(`el número ${Number(numP.value)} es IMPAR`);
+    //impresión en html
+    eje9.innerHTML = `el número ${Number(numP.value)} es IMPAR`;
+  }else{
+    alert(`el número ${Number(numP.value)} es CERO`);
+    //impresión en html
+    let eje9 = document.getElementById("eje9");
+    eje9.innerHTML = `el número ${Number(numP.value)} es CERO`;
+  }
+} */
 
 let btn10 = Array.from(document.querySelectorAll(".vm10"));
 for (let i = 0; i < btn10.length; i++) {
@@ -275,12 +309,12 @@ for (let i = 0; i < btn10.length; i++) {
     if (mos10 === "none") {
       dis10.style.display = "block";
       //operación
-      resultado = "pte";
-      //resultado
-      alert(`el resultado es ${resultado}`);
-      //impresión en html
-      let eje10 = document.getElementById("eje10");
-      eje10.innerHTML = resultado;
+      Number(numP.value < 0)
+        ? (alert(`El número ${Number(numP.value)} es NEGATIVO`),
+          (eje10 = document.getElementById("eje10")),
+          (eje10.innerHTML = `El número ${Number(numP.value)} es NEGATIVO`))
+        : (alert(`El número ${Number(numP.value)} es POSITIVO`),
+          (eje10.innerHTML = `El número ${Number(numP.value)} es POSITIVO`));
     } else {
       dis10.style.display = "none";
     }
@@ -294,13 +328,23 @@ for (let i = 0; i < btn11.length; i++) {
     let mos11 = dis11.style.display;
     if (mos11 === "none") {
       dis11.style.display = "block";
-      //operación
-      resultado = "pte";
-      //resultado
-      alert(`el resultado es ${resultado}`);
-      //impresión en html
-      let eje11 = document.getElementById("eje11");
-      eje11.innerHTML = resultado;
+
+      //operación d
+      let btnDb = document.getElementById("db");
+      btnDb.addEventListener("mouseover", () => {
+        resultado = Number(numP.value) * 2;
+        alert(`el resultado es ${resultado}`);
+        let eje11 = document.getElementById("eje11");
+        eje11.innerHTML = `el resultado es ${resultado}`;
+
+        let btnCd = document.getElementById("cd");
+        btnCd.addEventListener("mousedown", () => {
+          resultado = Number(numP.value) * 4;
+          alert(`el resultado es ${resultado}`);
+          let eje111 = document.getElementById("eje11--1");
+          eje111.innerHTML = `el resultado es ${resultado}`;
+        });
+      });
     } else {
       dis11.style.display = "none";
     }
@@ -355,8 +399,14 @@ for (let i = 0; i < btn14.length; i++) {
     if (mos14 === "none") {
       dis14.style.display = "block";
       //operación
-      resultado = "pte";
+      for (let i = 1; i < Number(numP.value); i++) {
+        if (i % 2 !== 0) {
+          Number((resultado = resultado + i));
+          console.log(resultado);
+        }
+      }
       //resultado
+      console.log(Number(numP.value));
       alert(`el resultado es ${resultado}`);
       //impresión en html
       let eje14 = document.getElementById("eje14");
@@ -386,6 +436,7 @@ for (let i = 0; i < btn15.length; i++) {
     }
   });
 }
+
 let btn16 = Array.from(document.querySelectorAll(".vm16"));
 for (let i = 0; i < btn16.length; i++) {
   btn16[i].addEventListener("click", () => {
@@ -406,22 +457,42 @@ for (let i = 0; i < btn16.length; i++) {
   });
 }
 
-let btn17 = Array.from(document.querySelectorAll(".vm17"));
+//Mostrar campos de eje17
+let btn17 = Array.from(document.querySelectorAll(".mm"));
 for (let i = 0; i < btn17.length; i++) {
   btn17[i].addEventListener("click", () => {
     let dis17 = document.querySelector(".res--17");
     let mos17 = dis17.style.display;
     if (mos17 === "none") {
       dis17.style.display = "block";
-      //operación
-      resultado = "pte";
-      //resultado
-      alert(`el resultado es ${resultado}`);
-      //impresión en html
-      let eje17 = document.getElementById("eje17");
-      eje17.innerHTML = resultado;
     } else {
       dis17.style.display = "none";
+    }
+  });
+}
+//operación eje17
+let btn17_1 = Array.from(document.querySelectorAll(".vm17"));
+for (let i = 0; i < btn17_1.length; i++) {
+  btn17_1[i].addEventListener("click", () => {
+    let numd = document.getElementById("numD");
+    if (Number(numP.value) % Number(numd.value) !== 0) {
+      alert(
+        `El número ${Number(numP.value)} no es divisible por ${Number(
+          numD.value
+        )}`
+      );
+      let eje17 = document.getElementById("eje17");
+      eje17.innerHTML = `El número ${Number(
+        numP.value
+      )} no es divisible por ${Number(numD.value)}`;
+    }else  if (Number(numP.value) % Number(numd.value) === 0){
+      alert(
+        `El número ${Number(numP.value)} si es divisible por ${Number(
+          numD.value
+        )}`
+      );
+      eje17.innerHTML = `El número ${Number(
+        numP.value)} si es divisible por ${Number(numD.value)}`;
     }
   });
 }
